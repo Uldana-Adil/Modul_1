cd26bad1de01fc88272348036c0f52b22356243e
Uldana-Adil <123613496+Uldana-Adil@users.noreply.github.com>
2023-10-23 11:45:50 +0600

    Add files via upload

---
+++ madul1/madul1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace madul1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine($"Приветствую тебя,{fullName}!");
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ task6/task6/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите несколько чисел через пробек: ");
+
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+
+            double sum = 0;
+            foreach (string number in numbers)
+            {
+                if (double.TryParse(number, out double parsedNumber))
+                {
+                    sum += parsedNumber;
+                }
+                else 
+                {
+                    Console.WriteLine("Ошибка.");
+                }
+            }
+            Console.WriteLine($"Сумма введенных чисел: {sum}");
+            Console.ReadKey();  
+        }
+    }
+}
